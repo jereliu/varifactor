2e1df26c3f3ca9d116df00d9eeb78422909ec26d
Jeremiah Zhe Liu <zhel90@gmail.com>
2018-02-21 13:32:40 -0500

    change to univariate normal family for faster sampling

--- varifactor/model.py
+++ varifactor/model.py
@@ -23,10 +23,18 @@ class NEFactorModel(object):
         :param track_transform: whether track transformed factors
         """
 
-        # fill in parameter
+        # fill in data parameter
         self.family = param.y['family']
         self.n, self.p = y.shape
         self.k = param.theta['k']
+
+        # fill in observed values
+        self.y = y
+        self.u = u
+        self.v = v
+        self.e = e
+
+        # fill in hyper-parameters
         self.eps_sd = param.theta['eps_sd']
         self.u_par = param.u
         self.v_par = param.v
@@ -42,13 +50,19 @@ class NEFactorModel(object):
         # check supplied u, v, and e
         if u is not None:
             if not u.shape == (self.k, self.n):
-                raise ValueError("provided u does not match model dimension (%d, %d)" % (self.k, self.n))
+                raise ValueError(
+                    "provided u does not match model dimension (%d, %d), "
+                    "either supply new u or change model dimension in param.theta['k']" % (self.k, self.n))
         if v is not None:
             if not v.shape == (self.k, self.p):
-                raise ValueError("provided v does not match model dimension (%d, %d)" % (self.k, self.p))
+                raise ValueError(
+                    "provided v does not match model dimension (%d, %d), "
+                    "either supply new u or change model dimension in param.theta['k']" % (self.k, self.p))
         if e is not None:
             if not e.shape == (self.n, self.p):
-                raise ValueError("provided e does not match outcome dimension (%d, %d)" % (self.n, self.p))
+                raise ValueError(
+                    "provided e does not match outcome dimension (%d, %d), "
+                    "either supply new u or change model dimension in param.theta['k']" % (self.n, self.p))
 
 
         # initialize model
@@ -60,28 +74,25 @@ class NEFactorModel(object):
         with nef_factor:
 
             # initialize random variables
-            u = pm.MvNormal(name="U",
-                            mu=0, cov=self.u_par['sd']**2 * np.eye(self.n),
-                            shape=(self.k, self.n),
-                            observed=u,
-                            testval=
-                            0 * np.random.normal(
+            u = pm.Normal(name="U",
+                          mu=0, sd=self.u_par['sd'],
+                          shape=(self.k, self.n), observed=u,
+                          testval=
+                          0 * np.random.normal(
                                 loc=0, scale=self.u_par['sd'],
-                                size=(self.k, self.n)),
-                            )
-            v = pm.MvNormal(name="V",
-                            mu=0, cov=self.v_par['sd']**2 * self.v_cov,
-                            shape=(self.k, self.p),
-                            observed=v,
-                            testval=
-                            0 * np.random.normal(
+                                size=(self.k, self.n))
+                          )
+            v = pm.Normal(name="V",
+                          mu=0, sd=self.v_par['sd'],
+                          shape=(self.k, self.p), observed=v,
+                          testval=
+                          0 * np.random.normal(
                                 loc=0, scale=self.v_par['sd'],
                                 size=(self.k, self.p)),
-                            )
+                          )
             e = pm.Normal(name="e",
                           mu=0, sd=1,
-                          shape=(self.n, self.p),
-                          observed=e,
+                          shape=(self.n, self.p), observed=e,
                           testval=
                           0 * np.random.normal(
                               loc=0, scale=self.eps_sd,
@@ -92,12 +103,13 @@ class NEFactorModel(object):
             if track_transform:
                 u_t = pm.Deterministic("Ut", _factor_transform(u.T, name=self.u_par["transform"]))
                 v_t = pm.Deterministic("Vt", _factor_transform(v.T, name=self.v_par["transform"]))
+                theta = pm.Deterministic("theta", tensor.dot(u_t, v_t.T) + e)
             else:
                 u_t = _factor_transform(u.T, name=self.u_par["transform"])
                 v_t = _factor_transform(v.T, name=self.v_par["transform"])
+                theta = tensor.dot(u_t, v_t.T) + e
 
             # theta
-            theta = pm.Deterministic("theta", tensor.dot(u_t, v_t.T) + e)
             y = _nef_family(name="y", theta=theta, n=self.n, p=self.p,
                             family=self.family, observed=y)
 
@@ -105,12 +117,68 @@ class NEFactorModel(object):
         logging.info('\n====================')
 
         self.model = nef_factor
-        # initialize gradient and hessian functions
-        # self.grad = nef_factor.dlogp()
-        # self.hess = nef_factor.d2logp()
+
+    def eigrad(self, u, native=False):
+        """
+        produce gradient function with respect to U
+        :param u:
+        :param native: whether to use build-in pymc3 function dlogp to calculate gradient
+        :return:
+        """
+        # produce gradient function with respect to U
+
+        # first compute d_L/d_U, a N x K matrix
+        if native:
+            # use example: grad_fun({'U': u_train.T})
+            dL_dU = self.model.dlogp()({'U': u.T})
+        else:
+            dL_dU = self._grad_LU(u)
+
+        # then compute d_U/d_eig, a rank x N x K matrix
+        dU_deig = self._grad_Ud(u)
+
+        # compute product:
+        rank = dU_deig.shape[0]
+        dL_deig = np.zeros(shape=rank)
+        for d in range(rank):
+            dL_deig[d] = np.mean(dL_dU * dU_deig[d, :, :])
+
+        return dL_deig
+
+    def _grad_LU(self, u):
+        """
+        :param U: a N x K matrix
+        :return:
+        """
+        # explicit calculation for gradient wrt U
+        y = self.y
+        v = self.v
+        theta = u.dot(v)
+
+        dA = _nef_partition_deriv(theta)
+        dL = (y - dA).dot(v.T) - u/self.u_par['sd']**2
+        return dL
+
+    def _grad_Ud(self, u):
+        """
+        :param U: a N x K matrix
+        :return:
+        """
+        # explicit calculation for gradient wrt U
+        l, d, r = np.linalg.svd(u, full_matrices=False)
+
+        dU = np.zeros((len(d),) + u.shape)
+
+        for i in range(len(d)):
+            dU[i, :, :] += d[i] * l[:, i, np.newaxis].dot(r[i, np.newaxis])
+
+        return dU
 
 
-# helper functions
+##################################
+# helper functions ###############
+##################################
+
 def _factor_transform(y, name="identity"):
     if name == "identity":
         return y
@@ -138,13 +206,13 @@ def _nef_family(theta, n, p, observed, family="Gaussian", name="y"):
 
     if family == "Gaussian":
         mu_par = theta
-        y = pm.Normal(mu=mu_par, sd=np.ones((n, p)), shape=(n, p),
-                      observed=observed, name=name + "_gaussian")
+        y = pm.Normal(mu=mu_par, sd=1, shape=(n, p),
+                      observed=observed, name=name)
 
     elif family == "Poisson":
         lambda_par = pm.math.exp(theta)
         y = pm.Poisson(mu=lambda_par, shape=(n, p),
-                       observed=observed, name=name + "_poisson")
+                       observed=observed, name=name)
 
     elif family == "Binomial":
         # TODO: give options to specify binom_n
@@ -154,9 +222,61 @@ def _nef_family(theta, n, p, observed, family="Gaussian", name="y"):
         p_par = 1/(1 + pm.math.exp(-theta))
         y = pm.Binomial(n=n_binom, p=p_par,
                         shape=(n, p), observed=observed,
-                        name=name + "_binom_" + str(n_binom))
+                        name=name)
 
     else:
         raise ValueError('distribution family (' + str(family) + ') not supported')
 
     return y
+
+
+def _nef_partition_deriv(theta, family="Gaussian"):
+    """
+    :param theta: value of natural parameter
+    :param n: sample size
+    :param family: distribution name
+    :return:
+    """
+
+    if family == "Gaussian":
+        deriv = theta
+
+    elif family == "Poisson":
+        deriv = np.exp(theta)
+
+    elif family == "Binomial":
+        # TODO: give options to specify binom_n
+        n_binom = 10
+        logging.warn('n is fixed to ' + str(n_binom) + ' for Binomial(n, p)')
+        deriv = n_binom * 1/(1 + np.exp(-theta))
+
+    else:
+        raise ValueError('distribution family (' + str(family) + ') not supported')
+
+    return deriv
+
+
+if __name__ == "__main__":
+    import pymc3 as pm
+    n = 100
+    p = 20
+    k = 5
+
+    u_obs = np.random.normal(size=(n, k))
+    v_obs = np.random.normal(size=(p, k))
+    y_obs = u_obs.dot(v_obs.T)
+
+    with pm.Model() as model:
+        u = pm.Normal('u', mu=0, sd=1, shape=(n, k))
+        theta = u.dot(v_obs.T)
+        y = pm.Normal('y', mu=theta, sd=1, shape=(n, p), observed=y_obs)
+
+    grad = model.dlogp()
+
+    u_test = np.random.normal(size=(n, k))
+    grad({'u': u_test})
+    (y_obs - u_test.dot(v_obs.T)).dot(v_obs) - u_test
+
+
+    grad2({'U': u_test.T}).reshape(5, 100).T - ((y_obs - u_test.dot(v_obs.T)).dot(v_obs) - u_test * 100)
+    grad({'u': u_test}).reshape(100, 5) - ((y_obs - u_test.dot(v_obs.T)).dot(v_obs) - u_test)
