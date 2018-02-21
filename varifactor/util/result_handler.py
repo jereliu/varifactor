76c81fca528f8f74175ad0c2ee03c907e3997008
Jeremiah Zhe Liu <zhel90@gmail.com>
2018-02-21 16:17:55 -0500

    added result handler for VI

--- varifactor/util/result_handler.py
+++ varifactor/util/result_handler.py
@@ -23,6 +23,22 @@ def get_sample(result, varname="U"):
     :param varname: target variable name
     :return: np.array of dimension chain_id x iteration x matrix dimension
     """
-    stat = result.get_values(varname, combine=False)
+    # get result
+    if result.method_type == "vi":
+        stat = [x[varname] for x in result.sample_tracker if x is not None]
+        stat = np.moveaxis(stat, 0, 1)
+        # if only one chain, then remove the chain dimension
+        if stat.shape[0] == 1:
+            stat = stat[0]
+
+    elif result.method_type == "mc":
+        stat = result.get_values(varname, combine=False)
+
+    # convert to array
     stat = np.asarray(stat)  # chain_id x iteration x matrix dimension
+
+    # adjust matrix dimension
+    if varname in ["U", "V"]:
+        stat = np.moveaxis(stat, -2, -1)
+
     return stat
