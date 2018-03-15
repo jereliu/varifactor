e6f65c29e9cd6f1032b1e717a939e2f3f8874209
Jeremiah Zhe Liu <zhel90@gmail.com>
2018-03-14 21:05:22 -0400

    vi_freq now is algorithm specific

--- varifactor/setting.py
+++ varifactor/setting.py
@@ -3,25 +3,24 @@ from collections import namedtuple
 param_model = {
     'y': {'family': 'Poisson'},
     'theta': {'k': 2, 'eps_sd': 0},
-    'u': {'sd': 0.2, 'transform': 'identity'},
-    'v': {'sd': 2, 'transform': 'identity', 'cov': None}
+    'u': {'sd': 1, 'transform': 'identity'},
+    'v': {'sd': 1, 'transform': 'identity', 'cov': None}
     }
 
 
 param_infer = {
     'chains': 1,
-    'n': 1000,
-    'tune': 1000,
-    'vi_freq': 200,   # extra iteration to run for VI
+    'n': 2000,
+    'tune': 2000,
     'method': 'NUTS',
     'start': 'zero', # str (MAP / zero) indicating init method, or a dict of actual init values
     'setting': {
         'Metropolis': {'scaling': 100.},
+        'Slice': {'scaling': 100.},
         'NUTS': {'target_accept': 0.8, 'max_treedepth': 10},
-        'ADVI': {'vi_freq': 1000},
-        'NFVI': {'flow': 'planar*10', 'jitter': 0.1, 'vi_freq': 1000},
-        'SVGD': {},
-        'OPVI': {}
+        'ADVI': {'vi_freq': 200},
+        'NFVI': {'flow': 'planar*8', 'jitter': 1., 'vi_freq': 50},
+        'SVGD': {'n_particles': 500, 'jitter': 1., 'vi_freq': 10}
     }
 }
 
